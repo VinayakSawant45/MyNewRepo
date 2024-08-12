841a432273bce29d10899aead587e7fc5f3893fc
chakradhar <chakradhark830@gmail.com>
2024-08-12 19:29:42 +0530

    chakradhar commit

--- src/main/java/com/jsp/SpringBootCRUD/SpringBootCrudApplication.java
+++ src/main/java/com/jsp/SpringBootCRUD/SpringBootCrudApplication.java
@@ -16,5 +16,6 @@ public class SpringBootCrudApplication {
 	@PostConstruct
 	public void printProperties() {
 		System.out.println("server port: " + serverPort);
+		System.out.println("chakradhar changes");
 	}
 }
